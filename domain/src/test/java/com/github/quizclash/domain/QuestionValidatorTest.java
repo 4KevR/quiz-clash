018617f753540bc2185b5d7eb803915c9c6882c7
4KevR <50447092+4KevR@users.noreply.github.com>
2024-03-22 18:21:05 +0100

    unit test setup + jacoco + README

---
+++ domain/src/test/java/com/github/quizclash/domain/QuestionValidatorTest.java
@@ -0,0 +1,33 @@
+package com.github.quizclash.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QuestionValidatorTest {
+    @Test
+    void validateForCorrectQuestionOptions() throws InvalidQuestionFormatException {
+        QuestionOption[] questionOptions = new QuestionOption[4];
+        questionOptions[0] = new QuestionOption("Test 1", true);
+        questionOptions[1] = new QuestionOption("Test 2", false);
+        questionOptions[2] = new QuestionOption("Test 3", false);
+        questionOptions[3] = new QuestionOption("Test 4", false);
+        assertEquals(QuestionValidator.validate(questionOptions), questionOptions);
+    }
+
+    @Test
+    void validateForIncorrectAmountOfQuestions() {
+        QuestionOption[] questionOptions = new QuestionOption[5];
+        assertThrows(InvalidQuestionFormatException.class, () -> QuestionValidator.validate(questionOptions));
+    }
+
+    @Test
+    void validateForMoreThanOneCorrectAnswer() {
+        QuestionOption[] questionOptions = new QuestionOption[4];
+        questionOptions[0] = new QuestionOption("Test 1", true);
+        questionOptions[1] = new QuestionOption("Test 2", true);
+        questionOptions[2] = new QuestionOption("Test 3", false);
+        questionOptions[3] = new QuestionOption("Test 4", false);
+        assertThrows(InvalidQuestionFormatException.class, () -> QuestionValidator.validate(questionOptions));
+    }
+}
