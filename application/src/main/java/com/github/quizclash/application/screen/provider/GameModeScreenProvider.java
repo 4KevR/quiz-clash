afd7e95819805726b46466997a8838fc9556718e
Julian <66846059+Julian702@users.noreply.github.com>
2024-06-07 10:25:58 +0200

    Replace OptionScreen creation with new MenuCreator

--- application/src/main/java/com/github/quizclash/application/screen/provider/GameModeScreenProvider.java
+++ application/src/main/java/com/github/quizclash/application/screen/provider/GameModeScreenProvider.java
@@ -1,35 +1,27 @@
 package com.github.quizclash.application.screen.provider;
 
-import com.github.quizclash.application.screen.OptionScreen;
 import com.github.quizclash.application.screen.ScreenFactory;
 import com.github.quizclash.application.screen.menu.GameModeEnum;
+import com.github.quizclash.application.screen.menu.MenuCreator;
 import com.github.quizclash.domain.Repository;
 
-import java.util.List;
-
 public class GameModeScreenProvider implements ScreenProvider {
-  private final Repository repository;
   private final ScreenFactory screenFactory;
   private ScreenProviderType nextScreenProviderType;
 
   public GameModeScreenProvider(Repository repository, ScreenFactory screenFactory) {
-    this.repository = repository;
     this.screenFactory = screenFactory;
   }
 
   @Override
   public void execute() {
-    int selection = 0;
-    while (selection <= 0 || selection > 4) {
-      OptionScreen optionScreen = screenFactory.createOptionScreen("Select Game Mode",
-          List.of(GameModeEnum.values()));
-      optionScreen.render();
-      selection = optionScreen.getOptionInput().getActionValue();
-      switch (selection) {
-        case 1 -> this.nextScreenProviderType = ScreenProviderType.TRAINING;
-        case 2 -> this.nextScreenProviderType = ScreenProviderType.LOCAL_MULTIPLAYER;
-        case 3 -> this.nextScreenProviderType = ScreenProviderType.ONLINE_MULTIPLAYER;
-      }
+    MenuCreator menuCreator = new MenuCreator("Select Game Mode", GameModeEnum.values(),
+        screenFactory);
+    int selection = menuCreator.displayAndGetSelection();
+    switch (selection) {
+      case 1 -> this.nextScreenProviderType = ScreenProviderType.TRAINING;
+      case 2 -> this.nextScreenProviderType = ScreenProviderType.LOCAL_MULTIPLAYER;
+      case 3 -> this.nextScreenProviderType = ScreenProviderType.ONLINE_MULTIPLAYER;
     }
   }
 
--- application/src/main/java/com/github/quizclash/application/screen/provider/UserMenuScreenProvider.java
+++ application/src/main/java/com/github/quizclash/application/screen/provider/UserMenuScreenProvider.java
@@ -1,41 +1,27 @@
 package com.github.quizclash.application.screen.provider;
 
-import com.github.quizclash.application.screen.OptionScreen;
 import com.github.quizclash.application.screen.ScreenFactory;
+import com.github.quizclash.application.screen.menu.MenuCreator;
 import com.github.quizclash.application.screen.menu.UserMenuEnum;
 import com.github.quizclash.domain.Repository;
 
-import java.util.List;
-
 public class UserMenuScreenProvider implements ScreenProvider {
-  private final Repository repository;
   private final ScreenFactory screenFactory;
   private ScreenProviderType nextScreenProviderType;
 
   public UserMenuScreenProvider(Repository repository, ScreenFactory screenFactory) {
-    this.repository = repository;
     this.screenFactory = screenFactory;
   }
 
   @Override
   public void execute() {
-    int selection = 0;
-    String menuTitle = "What do you want to do?";
-    while (selection <= 0 || selection > 3) {
-      OptionScreen optionScreen = screenFactory.createOptionScreen(menuTitle,
-          List.of(UserMenuEnum.values()));
-      optionScreen.render();
-      selection = optionScreen.getOptionInput().getActionValue();
-      switch (selection) {
-        case 1:
-          this.nextScreenProviderType = ScreenProviderType.ADD_USER;
-          break;
-        case 2:
-          this.nextScreenProviderType = ScreenProviderType.REMOVE_USER;
-          break;
-        default:
-          this.nextScreenProviderType = ScreenProviderType.MENU;
-      }
+    MenuCreator menuCreator = new MenuCreator("What do you want to do?", UserMenuEnum.values(),
+        screenFactory);
+    int selection = menuCreator.displayAndGetSelection();
+    switch (selection) {
+      case 1 -> this.nextScreenProviderType = ScreenProviderType.ADD_USER;
+      case 2 -> this.nextScreenProviderType = ScreenProviderType.REMOVE_USER;
+      default -> this.nextScreenProviderType = ScreenProviderType.MENU;
     }
   }
 
